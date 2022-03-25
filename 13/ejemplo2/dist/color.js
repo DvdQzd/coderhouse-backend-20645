ec877c0c6cc43b1ac9eeb43a11054767a6c253cd
DvdQzd <david.quezada.m@gmail.com>
2022-03-24 23:22:25 -0300

    clase 13

---
+++ 13/ejemplo2/dist/color.js
@@ -0,0 +1,10 @@
+var Color = /** @class */ (function () {
+    function Color() {
+    }
+    Color.prototype.getRandomColor = function () {
+        var randomValue = function () { return Math.floor(Math.random() * 256); };
+        return "rgb(".concat(randomValue(), ", ").concat(randomValue(), ", ").concat(randomValue(), ")");
+    };
+    return Color;
+}());
+export { Color };
